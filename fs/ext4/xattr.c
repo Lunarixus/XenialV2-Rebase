f623dd5473d5edd87db191aab60af00010b7be0f
Lunarixus <Nathanb644@hotmail.co.uk>
2020-05-28 17:05:17 +0000

    fs/ext4: xattr: Fix mismerge
    Signed-off-by: Lunarixus <Nathanb644@hotmail.co.uk>

--- fs/ext4/xattr.c
+++ fs/ext4/xattr.c
@@ -942,12 +942,8 @@ inserted:
 				if (ref >= EXT4_XATTR_REFCOUNT_MAX)
 					ce->e_reusable = 0;
 				ea_bdebug(new_bh, "reusing; refcount now=%d",
-<<<<<<< HEAD
 					le32_to_cpu(BHDR(new_bh)->h_refcount));
 				ext4_xattr_block_csum_set(inode, new_bh);
-=======
-					  ref);
->>>>>>> e047b6d040dc... mbcache: add reusable flag to cache entries
 				unlock_buffer(new_bh);
 				error = ext4_handle_dirty_metadata(handle,
 								   inode,
